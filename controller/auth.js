3f32eaf00ee48c29e1acff74b95af497acbcea82
varun sharma <vs343171@gmail.com>
2023-03-19 13:27:34 +0530

    added validation to the routes

--- controller/auth.js
+++ controller/auth.js
@@ -1,6 +1,7 @@
 // **********global modules
 const bcrypt = require('bcryptjs');
 const flash=require("connect-flash");
+const {validationResult}=require("express-validator")
 
 // ************local modules
 const FoodProduct = require('../models/foodProduct');
@@ -11,6 +12,8 @@ const getLogin = (req, res, next) => {
   return res.render('auth/login', {
     docTitle: 'Login Form',
     path: '',
+    errorMessage:req.flash('errorMessage'),
+    _errorMessage:req.flash('_errorMessage')
   });
 };
 
@@ -21,6 +24,7 @@ const postLogin = (req, res, next) => {
   const Find = async () => {
     const user = await User.findOne({ email: email });
     if (!user) {
+      const result=await req.flash('errorMessage','Email do not match in database');
       return res.redirect('/login');
     }
     const bool_ans =await bcrypt.compare(password, user.password);
@@ -31,6 +35,7 @@ const postLogin = (req, res, next) => {
         return res.redirect('/');
       });
     } else {
+      const result = await req.flash('_errorMessage','Invalid password');
       return res.redirect('/login');
     }
   };
@@ -47,6 +52,10 @@ const getSignUp = (req, res, next) => {
   return res.render('auth/sign-up.ejs', {
     docTitle: 'Sign Up Page',
     path: '',
+    errorMessage1:req.flash('error'),
+    errorMessage:null,
+    _errorMessage:null,
+    __errorMessage:null
   });
 };
 
@@ -54,12 +63,43 @@ const postSignUp = (req, res, next) => {
   const email = req.body.userEmail;
   const password = req.body.userPassword;
   const cpassword = req.body.cUserPassword;
+  const {errors}=validationResult(req);
 
+  const _errors=[null,null,null];
+
+  for(let i=0;i<errors.length;i++)
+  {
+    if(errors[i].param=="userEmail")
+    {
+      _errors[0]=errors[i].msg;
+    }
+    else if(errors[i].param=="userPassword")
+    {
+      _errors[1]=errors[i].msg;
+    }
+    else if(errors[i].param=="cUserPassword")
+    {
+      _errors[2]=errors[i].msg;
+    }
+  }
+
+  if(errors.length>0)
+  {
+    return res.status(422).render('auth/sign-up',{
+      docTitle:"Sign Up Page",
+      path:'',
+      errorMessage1:req.flash('error'),
+      errorMessage:_errors[0],
+      _errorMessage:_errors[1],
+      __errorMessage:_errors[2]
+    })
+  }
   const Find = async () => {
     try {
       const result = await User.findOne({ email: email });
       if (result) 
       {
+        const result=await req.flash('error',"Email already exist in database");
         return res.redirect('/sign-up');
       }
       const hashedPass = await bcrypt.hash(password, 12);
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 // **************global packages
 const express = require('express');
-const { check, body } = require('express-validator/check');
+const { check, body } = require('express-validator');
 
 // *************local packages
 const authController = require('../controller/auth');
@@ -18,7 +18,26 @@ router.get('/sign-up', authController.getSignUp);
 
 // ***************for posting sign-up page
 
-router.post('/sign-up',authController.postSignUp);
+router.post(
+  '/sign-up',
+  [
+    check('userEmail').isEmail().withMessage('Please enter a valid email'),
+    body('userPassword')
+      .isLength({ min: 5 })
+      .withMessage('Please enter a password with atleast 5 characters'),
+    body('cUserPassword').custom((value,{req})=>{
+        if(value!==req.body.userPassword)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }).withMessage("please enter same password")
+  ],
+  authController.postSignUp
+);
 
 router.post('/logout', authController.postLogout);
 
